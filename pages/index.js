e54ab7580b6101f9777e24d5341e3b51b02bae00
Jose Alvarado <jose_alvarado17@hotmail.com>
2020-06-27 19:41:53 -0400

    :tada: Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  </>
+)
+
+export default Home
