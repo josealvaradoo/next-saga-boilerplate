67eb3c2c6ff19196fb6e40d22625954138a998ae
Jose Alvarado <jose_alvarado17@hotmail.com>
2020-06-27 22:08:05 -0400

    Fix some issues

--- pages/_app.js
+++ pages/_app.js
@@ -3,40 +3,41 @@ import Head from 'next/head'
 import PropTypes from 'prop-types'
 import { END } from 'redux-saga'
 import store from '../redux/store'
+import '../assets/scss/styles.scss'
 
 const App = ({ Component, pageProps }) => (
-  <div className="App">
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Component {...pageProps} />
-  </div>
+	<div className="App">
+		<Head>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<Component {...pageProps} />
+	</div>
 )
 
 // Sync redux store into server side
 App.getInitialProps = async ({ Component, ctx }) => {
-  const pageProps = {
-    ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
-  }
+	const pageProps = {
+		...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
+	}
 
-  if (ctx.req) {
-    ctx.store.dispatch(END)
-    await ctx.store.sagaTask.toPromise()
-  }
+	if (ctx.req) {
+		ctx.store.dispatch(END)
+		await ctx.store.sagaTask.toPromise()
+	}
 
-  return {
-    pageProps,
-  }
+	return {
+		pageProps,
+	}
 }
 
 App.propTypes = {
-  Component: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
-  pageProps: PropTypes.oneOfType([PropTypes.node, PropTypes.object]),
+	Component: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+	pageProps: PropTypes.oneOfType([PropTypes.node, PropTypes.object]),
 }
 
 App.defaultProps = {
-  Component: '',
-  pageProps: {},
+	Component: '',
+	pageProps: {},
 }
 
 export default store.withRedux(App)
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 
 const Home = () => (
-  <>
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-  </>
+	<>
+		<Head>
+			<title>Create Next App</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<p>It's working</p>
+	</>
 )
 
 export default Home
