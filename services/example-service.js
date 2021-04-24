fca614165fbc040c4754c931871d8099d892b496
Jose Alvarado <jose_alvarado17@hotmail.com>
2021-04-24 00:07:53 -0400

    :bug: Fix bug on services

--- services/example-service.js
+++ services/example-service.js
@@ -1,9 +1,11 @@
 import HttpRequest from './http-request'
 
-export default class ExampleService {
-  static async getAll() {
-    HttpRequest.endpoint = 'example'
-    const response = await HttpRequest.get()
+class ExampleService extends HttpRequest {
+  async getAll() {
+    this.setEndpoint('example')
+    const response = await this.get()
     return response
   }
 }
+
+export default ExampleService
--- services/http-request.js
+++ services/http-request.js
@@ -1,11 +1,29 @@
 import axios from 'axios'
 
-export default class HttpRequest {
+class HttpRequest {
   constructor() {
     this.endpoint = ''
+    this.response = {
+      data: null,
+      code: 200,
+      success: true,
+      message: '',
+      error: '',
+    }
   }
 
-  static async get(id = null) {
+  setEndpoint(endpoint) {
+    this.endpoint = endpoint
+  }
+
+  setResponse(response) {
+    this.response = {
+      ...this.response,
+      ...response,
+    }
+  }
+
+  async get(id = null) {
     try {
       let response = null
       if (id) {
@@ -22,14 +40,27 @@ export default class HttpRequest {
         })
       }
 
-      return response.data
+      this.setResponse({
+        data: response?.data,
+        code: response?.code,
+        message: response?.message,
+      })
+
+      return this.response
     } catch (err) {
-      console.log(err.message)
-      return err.response.data
+      console.log(err?.message)
+      this.setResponse({
+        data: id === null ? [] : null,
+        error: err?.message,
+        success: false,
+        code: 500,
+      })
+
+      return this.response
     }
   }
 
-  static async post(data) {
+  async post(data) {
     try {
       const response = await axios.post(`${process.env.API_URI}/${this.endpoint}`, data, {
         headers: {
@@ -37,40 +68,83 @@ export default class HttpRequest {
         },
       })
 
-      return response.data
+      this.setResponse({
+        data: response?.data,
+        code: response?.code,
+        message: response?.message,
+      })
+
+      return this.response
     } catch (err) {
-      console.log(err.message)
-      return err.response.data
+      console.log(err?.message)
+      this.setResponse({
+        data: null,
+        error: err?.message,
+        success: false,
+        code: 500,
+      })
+
+      return this.response
     }
   }
 
-  static async put(id, data) {
+  async put(id, data) {
     try {
-      const response = await axios.post(`${process.env.API_URI}/${this.endpoint}/${id}`, data, {
+      const response = await axios.put(`${process.env.API_URI}/${this.endpoint}/${id}`, data, {
         headers: {
           'Content-Type': 'application/json',
         },
       })
 
-      return response.data
+      this.setResponse({
+        data: response?.data,
+        code: response?.code,
+        message: response?.message,
+      })
+
+      return this.response
     } catch (err) {
-      console.log(err.message)
-      return err.response.data
+      console.log(err?.message)
+
+      this.setResponse({
+        data: null,
+        error: err?.message,
+        success: false,
+        code: 500,
+      })
+
+      return this.response
     }
   }
 
-  static async delete(id) {
+  async delete(id) {
     try {
-      const response = await axios.post(`${process.env.API_URI}/${this.endpoint}/${id}`, {
+      const response = await axios.delete(`${process.env.API_URI}/${this.endpoint}/${id}`, {
         headers: {
           'Content-Type': 'application/json',
         },
       })
 
-      return response.data
+      this.setResponse({
+        data: response?.data,
+        code: response?.code,
+        message: response?.message,
+      })
+
+      return this.response
     } catch (err) {
-      console.log(err.message)
-      return err.response.data
+      console.log(err?.message)
+
+      this.setResponse({
+        data: null,
+        error: err?.message,
+        success: false,
+        code: 500,
+      })
+
+      return this.response
     }
   }
 }
+
+export default HttpRequest
